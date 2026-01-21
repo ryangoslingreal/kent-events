94abf55f8a17c38e14e336387d4dca2a3a9b8b85
ryangoslingreal <alexander.sokolovich05@gmail.com>
2026-01-21 14:53:29 +0000

    fix: Improved server proxy
    Can now be specified in .env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const target = process.env.VITE_API_TARGET ?? "http://localhost:3001";
+
 // https://vite.dev/config/
 export default defineConfig({
   	plugins: [react()],
   		server: {
     		proxy: {
       			"/api": {
-        		target: "http://localhost:3001",
+        		target: target,
         		changeOrigin: true,
         		secure: false,
       		},
