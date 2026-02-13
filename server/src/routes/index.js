94daa0e99462b3d3b07260d34b8822d3b2314663
Sasha Sokolov <117118805+ryangoslingreal@users.noreply.github.com>
2026-02-13 15:47:54 +0000

    feat: Added events router in preparation for merge

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 // ! Import routers here:
 const authRouter = require("./auth");
+const eventsRouter = require("./events");
 
 // Feature route registration
 function registerRoutes(app) {
     // ! Register future routes here:
     app.use("/api/auth", authRouter);
+    app.use("/api/events", eventsRouter);
 }
 
 module.exports = { registerRoutes };
