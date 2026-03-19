2725a5bb1ce96daba87784f06c004f02677c5574
Sasha Ganhao <as2856@kent.ac.uk>
2026-03-19 15:41:08 +0000

    Update Login.jsx

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -17,7 +17,6 @@ function Login() {
         try {
             const res = await fetch("/api/auth/login", {
                 method: "POST",
-                credentials: "include",
                 headers: { "Content-Type": "application/json" },
                 credentials: "include",
                 body: JSON.stringify({ email, password })
