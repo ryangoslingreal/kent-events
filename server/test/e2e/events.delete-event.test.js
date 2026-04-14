0203e8471f7cbb4c622851d617e9b3ed91bdcce6
Sasha Ganhao <as2856@kent.ac.uk>
2026-04-14 15:54:23 +0100

    chore: updated remaining /events testing skeletons

--- server/test/e2e/events.delete-event.test.js
+++ server/test/e2e/events.delete-event.test.js
@@ -1,4 +1,5 @@
 import { beforeEach, describe, it, expect, afterEach } from "vitest";
+const inbox = require("../helpers/emailInbox.js");
 const { createTestAgent } = require("../helpers/testingUtils.js");
 const { cleanupTestUsers, cleanupTestEvents } = require("../helpers/dbTestingUtils.js");
 const { } = require("../helpers/authTestingUtils.js");
@@ -10,14 +11,19 @@ describe.sequential("api/events/delete-event", () => {
         
     // Import app before each test
     beforeEach( async () => {
-        app = (await import("../../src/app.js")).default;
+        inbox.reset();
+            
+        const {mockEmail} = await import("../setup/email.mock.js");
+        mockEmail();
+            
+        app = (await import("../../src/app.js")).default; // Import app AFTER mock
         agent = createTestAgent(app);
     });
     
     // Clean up db after each test
     afterEach( async () => {
-        await cleanupTestUsers();
         await cleanupTestEvents();
+        await cleanupTestUsers();
     });
 
     it.todo("returns 200 and deletes existing event", async () => {
