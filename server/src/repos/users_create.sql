e841b8d6e3bfd5ebe374cd7369e5de980bbfd6ee
Sasha Ganhao <as2856@kent.ac.uk>
2026-05-03 16:15:35 +0100

    chore: Updated users_create.sql

--- server/src/repos/users_create.sql
+++ server/src/repos/users_create.sql
@@ -5,10 +5,11 @@ CREATE TABLE users (
 
     email VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    account_type ENUM("student", "society", "kentUni", "ksu") NOT NULL,
 
     email_verified_at TIMESTAMP NULL DEFAULT NULL,
 
-    email_verification_token_hash CHAR(64) NULL DEFAULT NULL,
+    email_verification_code_hash CHAR(64) NULL DEFAULT NULL,
     email_verification_expires_at TIMESTAMP NULL DEFAULT NULL,
 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -17,6 +18,6 @@ CREATE TABLE users (
 
     CONSTRAINT uq_users_email UNIQUE (email),
 
-    INDEX ix_users_email_verification_token_hash (email_verification_token_hash),
+    INDEX ix_users_email_verification_code_hash (email_verification_code_hash),
     INDEX ix_users_email_verified_at (email_verified_at)
 ) ENGINE=InnoDB;
