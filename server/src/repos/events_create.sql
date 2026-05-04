06c7bb91c212160d8312c37a0e3b4b3a4ea126a5
Sasha Ganhao <as2856@kent.ac.uk>
2026-05-04 13:09:01 +0100

    chore: Updated events_create.sql

--- server/src/repos/events_create.sql
+++ server/src/repos/events_create.sql
@@ -1,26 +1,40 @@
 USE as2856;
 
-CREATE TABLE events(
+CREATE TABLE events (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    
+
   user_id BIGINT UNSIGNED NOT NULL,
   title VARCHAR(255) NOT NULL,
-  subtitle VARCHAR(255),
-  description VARCHAR(255) NOT NULL,
-    
-  image LONGBLOB,
-  event_date DATE NOT NULL,
-  event_time TIME NOT NULL,
-    
+  description TEXT NOT NULL,
+
+  date DATE NOT NULL,
+  start_time TIME NOT NULL,
+  end_time TIME NULL,
+
   location VARCHAR(255) NOT NULL,
-  tags JSON,
-  price DECIMAL(4, 2) NOT NULL, 
-  repeat_event ENUM('never', 'weekly', 'monthly') NOT NULL DEFAULT 'never',
-  available_contact BOOLEAN NOT NULL,
+  tags JSON NULL,
+
+  ticket_url TEXT NULL,
+  contact_email VARCHAR(255),
+
+  image LONGBLOB NULL,
+  image_mime VARCHAR(50) NULL,
     
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	ON UPDATE CURRENT_TIMESTAMP, 
+  image_url TEXT NULL,
+  background_image_url TEXT NULL,
+
+  external_url TEXT NULL,
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	  ON UPDATE CURRENT_TIMESTAMP, 
         
-	CONSTRAINT fk_events_user FOREIGN KEY (user_id) REFERENCES users(id)
+	CONSTRAINT fk_events_user
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE,
+
+  CONSTRAINT uq_events_external_url UNIQUE (external_url),
+
+  INDEX ix_events_user_id (user_id),
+  INDEX ix_events_date_start_time (date, start_time)
 ) ENGINE=InnoDB;
